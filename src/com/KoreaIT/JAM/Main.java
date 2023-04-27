3d9e627ff5db5ddce51493d33f33c12a99b13538
shinsangsoo <sangsoo9400@gmail.com>
2023-04-27 19:43:55 +0900

    article write, article list

--- src/com/KoreaIT/JAM/Main.java
+++ src/com/KoreaIT/JAM/Main.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.JAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +10,10 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		List<Article> articles = new ArrayList<>();
+		
+		int lastArticleId = 0;
+		
 		while(true) {
 			System.out.printf("명령어)");
 			String cmd = sc.nextLine().trim();
@@ -15,6 +21,37 @@ public class Main {
 			if(cmd.equals("exit")) {
 				System.out.println("== 프로그램 끝 ==");
 				break;
+								
+			}
+			if(cmd.equals("article write")) {
+				System.out.println("== 게시물 작성 ==");
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				
+				System.out.printf("%d번 글이 생성되었습니다\n", id);
+				
+				
+			} else if(cmd.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==");
+				
+				if(articles.size() == 0) {
+					System.out.println("존재하는 게시물이 없습니다");
+					continue;
+				}
+				
+				System.out.println("번호	|	제목");
+				
+				for(Article article : articles) {
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+				
 			}
 		}
 		
